4bc67dd56a58a374ae6077bc1766921390363d92
= <kendouciabdelali@gmail.com>
2023-03-26 15:11:24 +0100

    'clean'

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 // Routes
 const apiRoutes = require('./routes/Api.js')
 const authRoutes = require('./routes/Auth.js')
-// const auth = require('./routes/Auth.js')
+
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -14,10 +14,6 @@ app.use(bodyParser.json());
 // Enable this routes
 app.use('/api', apiRoutes)
 app.use('/auth', authRoutes)
-
-app.use(express.static(__dirname + '/public/'));
-app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-
 
 const PORT = process.env.PORT || 1880;
 app.listen(PORT, ()=>{
