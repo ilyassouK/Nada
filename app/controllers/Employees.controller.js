3bc66a62ae8dfba8781d598105f76e67208296ec
= <kendouciabdelali@gmail.com>
2023-04-04 20:43:36 +0100

    fix duplicated attending rows

--- app/controllers/Employees.controller.js
+++ app/controllers/Employees.controller.js
@@ -5,13 +5,7 @@ const limit = 30;
 
 
 controllers.selectEmployees = (req, res, next)=>{
-    // query = "SELECT id, full_name AS fullName FROM users WHERE role = 3 AND active = 1"
-    query = `SELECT id, 
-                    IFNULL(full_name, 
-                            CONCAT(first_name,' ',parent_name,' ',grand_father,' ',familly_name)
-                    ) AS fullName 
-            FROM users
-            WHERE role = 3 AND active = 1`
+    query = "SELECT id, full_name AS fullName FROM users WHERE role = 3 AND active = 1"
     return next();
 }
 module.exports = controllers;
