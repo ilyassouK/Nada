b7732afe2776f8168f7b482d439c7f13824557fe
= <kendouciabdelali@gmail.com>
2023-04-05 11:50:36 +0100

    DB password

--- app/config/DB.js
+++ app/config/DB.js
@@ -5,7 +5,7 @@ let con = mysql.createPool({
     connectionLimit : 10,
     host: 'localhost',
     user: 'root',
-    password: 'nadA@2030nada',
+    password: 'nadaDB@2030',
     database: 'nada_db'
 })
 module.exports = con;
