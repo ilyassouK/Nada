bf9adb99fadf9c32f4f0fb3b228f40c8c75b7bed
= <kendouciabdelali@gmail.com>
2023-05-02 21:58:55 +0100

    observed table

--- app/controllers/Products.controller.js
+++ app/controllers/Products.controller.js
@@ -443,9 +443,7 @@ controllers.fetchAttendedProducts = (req, res)=>{
                           COALESCE(users.full_name, 'لم يُحضر') AS employeeName
                           ${commonQuery}
 
-                          GROUP BY products.id, product_tracking.id, product_tracking.employee_id, product_tracking.product_id, product_tracking.client_id, 
-                          product_tracking.observed_at, product_tracking.status, clients.name, clients.trade_name, 
-                          transactions.receipt_date, items.name, items.id, users.full_name, transactions.id, transactions.client_id
+                          GROUP BY products.id
                           ORDER BY COALESCE(product_tracking.created_at, transactions.created_at) DESC
 
                           ${!limtLess ? `
