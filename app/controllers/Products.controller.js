7173405ceb7fb76725016fd863b147232cfcdfe3
= <kendouciabdelali@gmail.com>
2023-05-06 21:20:37 +0100

    observed&pagination

--- app/controllers/Products.controller.js
+++ app/controllers/Products.controller.js
@@ -389,13 +389,13 @@ controllers.fetchAttendedProducts = (req, res)=>{
                             SELECT product_id, observed_at AS observed_at, status AS status, employee_id AS employee_id
                             FROM product_tracking
                             GROUP BY product_id, observed_at
-                            ORDER BY observed_at ASC
+                            ORDER BY observed_at DESC
                           ) pt ON p.id = pt.product_id
                           LEFT JOIN (
                             SELECT product_id, observed_at AS searchedDate, status AS searchedStatus, employee_id AS searchedEmployee
                             FROM product_tracking
                             WHERE observed_at BETWEEN '${date} 00:00:00' AND '${date} 23:59:59'
-                            ORDER BY observed_at ASC
+                            ORDER BY observed_at DESC
                           ) sd ON p.id = sd.product_id
                           LEFT JOIN users u ON u.id = COALESCE(sd.searchedEmployee, pt.employee_id)
                           WHERE 1=1 
