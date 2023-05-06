52282339cd03471eae28c753c84c79154af5c763
= <kendouciabdelali@gmail.com>
2023-05-06 17:14:37 +0100

    observed&pagination

--- app/controllers/Products.controller.js
+++ app/controllers/Products.controller.js
@@ -386,12 +386,12 @@ controllers.fetchAttendedProducts = (req, res)=>{
                           JOIN transactions t ON p.id = t.product_id
                           JOIN clients c ON t.client_id = c.id
                           LEFT JOIN (
-                            SELECT product_id, observed_at AS observed_at, status AS status, employee_id AS employee_id
+                            SELECT product_id, MAX(observed_at) AS observed_at, MAX(status) AS status, MAX(employee_id) AS employee_id
                             FROM product_tracking
                             GROUP BY product_id, observed_at
                           ) pt ON p.id = pt.product_id
                           LEFT JOIN (
-                            SELECT product_id, observed_at AS searchedDate, status AS searchedStatus, employee_id AS searchedEmployee
+                            SELECT product_id, MAX(observed_at) AS searchedDate, MAX(status) AS searchedStatus, MAX(employee_id) AS searchedEmployee
                             FROM product_tracking
                             WHERE observed_at BETWEEN '${date} 00:00:00' AND '${date} 23:59:59'
                           ) sd ON p.id = sd.product_id
@@ -431,8 +431,7 @@ controllers.fetchAttendedProducts = (req, res)=>{
                             c.name AS clientName,
                             c.trade_name AS tradeName
                           ${commonQuery}
-                          GROUP BY p.id, p.item_id, i.name, observedAt, status, employeeName, sd.searchedEmployee, t.receipt_date, t.client_id, c.name, c.trade_name
-
+                          GROUP BY p.id
                           ORDER BY COALESCE(pt.observed_at, t.receipt_date) DESC
 
                           ${!limtLess ? `
