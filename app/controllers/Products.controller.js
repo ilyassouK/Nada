602aea8f61273b468f808947dc2dbfab07c8bf3b
= <kendouciabdelali@gmail.com>
2023-05-06 16:06:34 +0100

    observed&pagination

--- app/controllers/Products.controller.js
+++ app/controllers/Products.controller.js
@@ -386,12 +386,12 @@ controllers.fetchAttendedProducts = (req, res)=>{
                           JOIN transactions t ON p.id = t.product_id
                           JOIN clients c ON t.client_id = c.id
                           LEFT JOIN (
-                            SELECT product_id, observed_at AS observed_at, status AS status, employee_id AS employee_id
+                            SELECT product_id, MAX(observed_at) AS observed_at, status AS status, employee_id AS employee_id
                             FROM product_tracking
-                            GROUP BY product_id, observed_at
+                            GROUP BY product_id
                           ) pt ON p.id = pt.product_id
                           LEFT JOIN (
-                            SELECT product_id, MAX(observed_at) AS searchedDate, status AS searchedStatus, employee_id AS searchedEmployee
+                            SELECT product_id, observed_at AS searchedDate, status AS searchedStatus, employee_id AS searchedEmployee
                             FROM product_tracking
                             WHERE observed_at BETWEEN '${date} 00:00:00' AND '${date} 23:59:59'
                           ) sd ON p.id = sd.product_id
