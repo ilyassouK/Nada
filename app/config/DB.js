b524d007da380629b696666ee53a1e87fab75489
= <kendouciabdelali@gmail.com>
2023-05-06 22:14:06 +0100

    observed&pagination

--- app/config/DB.js
+++ app/config/DB.js
@@ -5,7 +5,7 @@ let con = mysql.createPool({
     connectionLimit : 10,
     host: 'localhost',
     user: 'root',
-    password: '',
+    // password: '',
     password: 'nadaDB@2030',
     database: 'nada_db'
 })
