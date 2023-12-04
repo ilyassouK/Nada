574575e77644b0279da9192e3a53f7e03dcc11bd
ilyassouK <ilyassou2013@gmail.com>
2023-12-04 22:21:11 +0100

    debug login

--- app/config/DB.js
+++ app/config/DB.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 // MySQL connection configurations
 let con = mysql.createPool({
     connectionLimit : 10,
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     // password: '',
     password: 'nadaDB@2030',
